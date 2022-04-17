2cc35c2f3a2d53e746bfae21bd8f7bbe78de2ced
Javier Sanchez <jsanchez0508@hotmail.com>
2022-04-16 23:43:07 -0500

    Eliminacion de vistas

--- sistema/cinejungla/urls.py
+++ sistema/cinejungla/urls.py
@@ -6,8 +6,7 @@ from django.contrib.staticfiles.urls import static
 
 urlpatterns = [
     path('', views.login, name='login'),
-    path('nosotros', views.nosotros, name='nosotros'),
-    path('libros', views.libros, name='libros'),
+    path('multiplex', views.consultar_multiplex, name='consultar_multiplex'),
     path('libros/crear', views.crear, name='crear'),
     path('libros/editar', views.editar, name='editar'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
